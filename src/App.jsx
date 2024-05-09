af537f21a1e6b922ac82a811e2e4c9ea89249e78
SACHIN PRAJAPATI <skprajapati3214@gmail.com>
2024-05-10 00:24:57 +0530

    selection range code is comented

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
 
   const copyPasswordToClipboard=useCallback(()=>{
     passwordRef.current?.select();
-    passwordRef.current?.setSelectionRange(0,3);
+    // passwordRef.current?.setSelectionRange(0,3);
     window.navigator.clipboard.writeText(password);
   },[password])
 
